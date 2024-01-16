936640333abb0ac9fdb3891e0748ca1801e3a800
grxxtseoyxxn <103996791+grxxtseoyxxn@users.noreply.github.com>
2024-01-16 13:45:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const imageElement = document.getElementById('dynamicImage');
+    const imageElement = document.querySelector('img');
 
-    function getCurrentImage() {
+    function updateImage() {
         const currentDate = new Date();
         const currentDay = currentDate.getDay(); // 0: 일요일, 1: 월요일, ..., 6: 토요일
         const currentHour = currentDate.getHours();
 
         // 월요일 7시에 첫 번째 이미지, 그 외에는 두 번째 이미지
-        if (currentDay === 1 && currentHour === 7) {
-            return 'https://images.beta.cosmos.so/6b66af99-9aca-4502-9fde-bbb6a330bf56?format=jpeg';
-        } else {
-            return 'https://images.beta.cosmos.so/f83f1dca-8f87-4e29-8aaf-195dbdde2c7e?format=jpeg';
-        }
-    }
+        const imageUrl = (currentDay === 1 && currentHour === 7) ?
+            'monday_image.jpg' :
+            'other_days_image.jpg';
 
-    function updateImage() {
-        const imageUrl = getCurrentImage();
         imageElement.src = imageUrl;
     }
 
