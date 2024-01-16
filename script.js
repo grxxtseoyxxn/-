5410769b539e3ec7d49e45190dda228fbef1c87a
grxxtseoyxxn <103996791+grxxtseoyxxn@users.noreply.github.com>
2024-01-16 13:37:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // GitHub에서 이미지 URL을 가져오는 함수
-    async function getImageUrl() {
-        try {
-            const response = await fetch('https://images.beta.cosmos.so/6b66af99-9aca-4502-9fde-bbb6a330bf56?format=jpeg');
-            const data = await response.json();
-            return data.imageUrl;
-        } catch (error) {
-            console.error('에러 발생:', error);
-            return null;
+    const imageElement = document.getElementById('dynamicImage');
+
+    function getCurrentImage() {
+        const currentDate = new Date();
+        const currentDay = currentDate.getDay(); // 0: 일요일, 1: 월요일, ..., 6: 토요일
+        const currentHour = currentDate.getHours();
+
+        // 월요일 7시에 첫 번째 이미지, 그 외에는 두 번째 이미지
+        if (currentDay === 1 && currentHour === 7) {
+            return 'https://images.beta.cosmos.so/6b66af99-9aca-4502-9fde-bbb6a330bf56?format=jpeg';
+        } else {
+            return 'https://images.beta.cosmos.so/f83f1dca-8f87-4e29-8aaf-195dbdde2c7e?format=jpeg';
         }
     }
 
-    // 이미지를 업데이트하는 함수
-    async function updateImage() {
-        const imageUrl = await getImageUrl();
-        if (imageUrl) {
-            document.getElementById('dynamicImage').src = imageUrl;
-        }
+    function updateImage() {
+        const imageUrl = getCurrentImage();
+        imageElement.src = imageUrl;
     }
 
     // 초기에 이미지 업데이트
@@ -25,3 +25,4 @@ document.addEventListener('DOMContentLoaded', function() {
     // 1분마다 이미지 업데이트
     setInterval(updateImage, 60000);
 });
+
